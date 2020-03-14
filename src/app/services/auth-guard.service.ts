fba39bfcca670abb0c2453c2dcf45ace4eeb6d89
s00181793 <s00181793@mail.itsligo.ie>
2020-03-14 22:22:47 +0000

    added auth gaurds

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuardService implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const isAuthenticated = !!localStorage.getItem('access_token');
+
+    if (!isAuthenticated) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+    return true;
+  }
+}
